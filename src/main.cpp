c1833ce58536cedfeafc17b8bde89190977a9571
shadowforce78 <planque.adam@gmail.com>
2024-01-24 23:41:51 +0100

    Button that shows text

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,25 @@ using namespace geode::prelude;
 
 class $modify(Test, PauseLayer){
     void onMySillyyBtn(CCObject * target){
-        FLAlertLayer::create(
-            "Geode",
-            "Hello World from my Custom Mod!",
-            "OK")
-            ->show();
+        // FLAlertLayer::create(
+        //     "Geode",
+        //     "Hello World from my Custom Mod!",
+        //     "OK")
+        //     ->show();
+
+        // Make a toggable text label
+        auto label = CCLabelBMFont::create("Hello World!", "bigFont.fnt");
+        label->setPosition(100, 100);
+        label->setScale(0.5);
+        label->setTag(1);
+        this->addChild(label);
+
+        
+
 }
 
-void customSetup(){
+void customSetup()
+{
     PauseLayer::customSetup();
     auto buttonSprite = CCSprite::createWithSpriteFrameName("GJ_stopEditorBtn_001.png");
     auto button = CCMenuItemSpriteExtra::create(
@@ -25,7 +36,7 @@ void customSetup(){
 
     auto menu = CCMenu::create();
     menu->addChild(button);
-    menu->setPosition(462,212);
+    menu->setPosition(462, 212);
     menu->setScale(0.75);
     this->addChild(menu);
 }
