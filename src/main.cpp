5acbb5661ba0554f51bd81614902a9210b9f04db
shadowforce78 <planque.adam@gmail.com>
2024-01-26 19:03:42 +0100

    Add toggable btn

--- src/main.cpp
+++ src/main.cpp
@@ -3,45 +3,60 @@
 #include <Geode/modify/PauseLayer.hpp>
 
 using namespace geode::prelude;
-//  "opcodes": [
-//         {"addr": "0x20A23C", "on": "E9 79 06 00 00", "off": "6A 14 8B CB FF"}
-//     ],
 
-class $modify(Test, PauseLayer){
-    void onMySillyyBtn(CCObject * target){
+class $modify(Test, PauseLayer)
+{
+
+    bool buttonPressed = false;
+
+    void onMySillyyBtn(CCObject * target)
+    {
         // FLAlertLayer::create(
         //     "Geode",
         //     "Hello World from my Custom Mod!",
         //     "OK")
         //     ->show();
 
-        // Make a toggable text label
-        auto label = CCLabelBMFont::create("Hello World!", "bigFont.fnt");
-        label->setPosition(100, 100);
-        label->setScale(0.5);
-        label->setTag(1);
-        this->addChild(label);
+        // Toggle the button
+        if (buttonPressed == false)
+        {
+            buttonPressed = true;
+            auto label = CCLabelBMFont::create("Noclip Enabled", "bigFont.fnt");
+            label->setPosition(71, 272);
+            label->setScale(0.5);
+            label->setTag(1);
+            label->setVisible(buttonPressed);
+            this->addChild(label);
+            //  "opcodes": [
+            //         {"addr": "0x20A23C", "on": "E9 79 06 00 00", "off": "6A 14 8B CB FF"}
+            //     ],
 
-        
+            
 
-}
+        }
+        else
+        {
+            buttonPressed = false;
+            this->removeChildByTag(1);
 
-void customSetup()
-{
-    PauseLayer::customSetup();
-    auto buttonSprite = CCSprite::createWithSpriteFrameName("GJ_stopEditorBtn_001.png");
-    auto button = CCMenuItemSpriteExtra::create(
-        buttonSprite, nullptr, this,
-        menu_selector(Test::onMySillyyBtn));
+        }
+    }
 
-    auto menu = CCMenu::create();
-    menu->addChild(button);
-    menu->setPosition(462, 212);
-    menu->setScale(0.75);
-    this->addChild(menu);
-}
-}
-;
+    void customSetup()
+    {
+        PauseLayer::customSetup();
+        auto buttonSprite = CCSprite::createWithSpriteFrameName("GJ_stopEditorBtn_001.png");
+        auto button = CCMenuItemSpriteExtra::create(
+            buttonSprite, nullptr, this,
+            menu_selector(Test::onMySillyyBtn));
+
+        auto menu = CCMenu::create();
+        menu->addChild(button);
+        menu->setPosition(462, 212);
+        menu->setScale(0.75);
+        this->addChild(menu);
+    }
+};
 
 class $modify(MyAwesomeModification, MenuLayer){
 
